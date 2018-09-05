738bb8ffe1b266497c37aa42cc74c97a1dc4710c
West Kraemer <kraemerwest@gmail.com>
2018-09-05 11:55:57 -0400

    add nav bar items

---
+++ tableViewz/AddItemViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AddItemViewController.swift
+//  tableViewz
+//
+//  Created by West Kraemer on 9/5/18.
+//  Copyright © 2018 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.largeTitleDisplayMode = .never
+        
+    }
+    
+    
+}
+
